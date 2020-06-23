a529ed462bca5298db7fd267385556542258ce89
Devyash <bordia999@gmail.com>
2020-06-24 02:52:19 +0530

    Updates

---
+++ precache-manifest.1eaa7522f710896d3363dc84362a91ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cf6cac5d2aa34eccc87cd17805ecc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d0d19ff583a929e218c",
+    "url": "/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "f202a8280cc54581c7bb",
+    "url": "/static/css/main.71bf6fcd.chunk.css"
+  },
+  {
+    "revision": "3d0d19ff583a929e218c",
+    "url": "/static/js/2.19c6dbab.chunk.js"
+  },
+  {
+    "revision": "0f71eac5ee2a915954f46c4d6c83e759",
+    "url": "/static/js/2.19c6dbab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f202a8280cc54581c7bb",
+    "url": "/static/js/main.303f08f9.chunk.js"
+  },
+  {
+    "revision": "aea6449670cda39d86c4",
+    "url": "/static/js/runtime-main.013c5f84.js"
+  },
+  {
+    "revision": "cb9f48d45382e5e67dd83d084516c452",
+    "url": "/static/media/1_2_devyash_photo.cb9f48d4.jpg"
+  }
+]);
